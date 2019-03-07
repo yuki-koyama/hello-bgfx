a0cf1638cf5708dcf1d0e0c7f55c447c95dfc170
Yuki Koyama <yuki@koyama.xyz>
2019-03-07 22:55:32 +0900

    Link GLFW

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <bgfx/bgfx.h>
+#include <GLFW/glfw3.h>
 #include <dear-imgui/imgui.h>
 
 int main(int argc, char** argv)
 {
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Hello bgfx!", nullptr, nullptr);
     bgfx::init();
     return 0;
 }

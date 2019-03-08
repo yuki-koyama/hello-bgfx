7ad73ade8c5e169cd30b71e2cf55106ed3dc6201
Yuki Koyama <yuki@koyama.xyz>
2019-03-08 21:41:51 +0900

    Separate files

--- main.cpp
+++ main.cpp
@@ -2,12 +2,7 @@
 #include <iostream>
 #include <string>
 #include <bx/math.h>
-#include <bgfx/bgfx.h>
-#include <GLFW/glfw3.h>
-#define GLFW_EXPOSE_NATIVE_COCOA
-#include <GLFW/glfw3native.h>
-
-#define FIX_MACOS_MOJAVE_ISSUE
+#include "templatewindow.hpp"
 
 bgfx::ShaderHandle loadShader(const std::string& shader_name)
 {
@@ -52,108 +47,6 @@ struct Vertex
     float y;
     float z;
     uint32_t abgr;
-};
-
-class TemplateWindow
-{
-public:
-    TemplateWindow(const std::string& title, const int width = 640, const int height = 480)
-    {
-        if (!glfwInit())
-        {
-            throw std::runtime_error("Cannot initialize GLFW.");
-        }
-
-        m_window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
-
-        if (!m_window) { throw std::runtime_error("Cannot create a window."); }
-    }
-
-    ~TemplateWindow()
-    {
-        if (m_is_initialized)
-        {
-            bgfx::shutdown();
-        }
-        glfwDestroyWindow(m_window);
-        glfwTerminate();
-    }
-
-    void run()
-    {
-        const int width = getWidth();
-        const int height = getHeight();
-
-        bgfx::Init bgfx_init_settings;
-        bgfx_init_settings.type = bgfx::RendererType::Metal;
-        bgfx_init_settings.resolution.width = width;
-        bgfx_init_settings.resolution.height = height;
-        bgfx_init_settings.resolution.reset = BGFX_RESET_VSYNC;
-        bgfx_init_settings.platformData.nwh = glfwGetCocoaWindow(m_window);
-        bgfx::init(bgfx_init_settings);
-
-        initializeGraphics();
-
-        m_is_initialized = true;
-
-#ifdef FIX_MACOS_MOJAVE_ISSUE
-        bool is_initialized_for_mojave = false;
-#endif
-
-        m_counter = 0;
-        while (!glfwWindowShouldClose(m_window))
-        {
-#ifdef FIX_MACOS_MOJAVE_ISSUE
-            if (!is_initialized_for_mojave)
-            {
-                constexpr int non_zero = 1;
-                glfwPollEvents();
-                glfwSetWindowSize(m_window, width + non_zero, height);
-                glfwSetWindowSize(m_window, width, height);
-                is_initialized_for_mojave = true;
-            }
-#endif
-
-            updateGraphics();
-            bgfx::frame();
-            glfwPollEvents();
-            ++ m_counter;
-        }
-    }
-
-    GLFWwindow* getWindow() const { return m_window; }
-
-    int getWidth() const
-    {
-        int width, height;
-        glfwGetWindowSize(m_window, &width, &height);
-        return width;
-    }
-
-    int getHeight() const
-    {
-        int width, height;
-        glfwGetWindowSize(m_window, &width, &height);
-        return height;
-    }
-
-    float getAspect() const
-    {
-        int width, height;
-        glfwGetWindowSize(m_window, &width, &height);
-        return static_cast<float>(width) / static_cast<float>(height);
-    }
-
-protected:
-
-    virtual void initializeGraphics() = 0;
-    virtual void updateGraphics() = 0;
-
-    uint32_t m_counter;
-
-    GLFWwindow* m_window;
-
-    bool m_is_initialized = false;
 };
 
 class Window : public TemplateWindow

0083d3221c041caaff11fb1216f14edcfddc6ef9
Yuki Koyama <yuki@koyama.xyz>
2019-03-08 09:19:13 +0900

    Setup the event loop and fix macos mojave-specific issue

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <bgfx/bgfx.h>
-#include <bgfx/platform.h>
 #include <GLFW/glfw3.h>
 #define GLFW_EXPOSE_NATIVE_COCOA
 #include <GLFW/glfw3native.h>
-#include <dear-imgui/imgui.h>
+
+#define FIX_MACOS_MOJAVE_ISSUE
 
 namespace
 {
@@ -13,19 +13,59 @@ namespace
 
 int main(int argc, char** argv)
 {
-    glfwInit();
+    if (!glfwInit())
+    {
+        return -1;
+    }
+
     GLFWwindow* window = glfwCreateWindow(window_width, window_height, "Hello bgfx!", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
 
-    bgfx::PlatformData platform_data;
-    platform_data.nwh = glfwGetCocoaWindow(window);
-    bgfx::setPlatformData(platform_data);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
-    bgfx::Init bgfxInit;
-    bgfxInit.type = bgfx::RendererType::Count;
-    bgfxInit.resolution.width = window_width;
-    bgfxInit.resolution.height = window_height;
-    bgfxInit.resolution.reset = BGFX_RESET_VSYNC;
-    bgfx::init(bgfxInit);
+    bgfx::Init bgfx_init_settings;
+    bgfx_init_settings.type = bgfx::RendererType::Count;
+    bgfx_init_settings.resolution.width = window_width;
+    bgfx_init_settings.resolution.height = window_height;
+    bgfx_init_settings.resolution.reset = BGFX_RESET_VSYNC;
+    bgfx_init_settings.platformData.nwh = glfwGetCocoaWindow(window);
+    bgfx::init(bgfx_init_settings);
+
+    bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x443355FF, 1.0f, 0);
+    bgfx::setViewRect(0, 0, 0, window_width, window_height);
+
+    glfwHideWindow(window);
+    glfwShowWindow(window);
+
+#ifdef FIX_MACOS_MOJAVE_ISSUE
+    bool is_initialized_for_mojave = false;
+#endif
+
+    while (!glfwWindowShouldClose(window))
+    {
+#ifdef FIX_MACOS_MOJAVE_ISSUE
+        if (!is_initialized_for_mojave)
+        {
+            glfwPollEvents();
+            glfwSetWindowSize(window, window_width + 1, window_height);
+            glfwSetWindowSize(window, window_width, window_height);
+            is_initialized_for_mojave = true;
+        }
+#endif
+
+        bgfx::frame();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    bgfx::shutdown();
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
